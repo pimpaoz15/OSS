706374ccf462e91c654ede29b9f4079742aa1e2b
pimpaoz15 <120665865+pimpaoz15@users.noreply.github.com>
2023-09-19 17:06:48 +0200

    small main changes

--- L1/test_main.cpp
+++ L1/test_main.cpp
@@ -7,26 +7,23 @@ extern "C"
 #include "func_to_test.h"
 }
 
-// Test squareRoot of 123.145 with 1e-6 tolerance
+// Test the squareRoot function with the value 123.145 with 1e-6 tolerance
 TEST(SquareRootTest, SquareRootTest1)
 {
-    // Test the squareRoot function with the value 123.145
     double result = squareRoot(123.145);
     EXPECT_NEAR(result, 11.097072, 1e-6);
 }
 
-// Test squareRoot of 63924.123566 with 1e-6 tolerance
+// Test the squareRoot function with the value 63924.12356 with 1e-6 tolerance
 TEST(SquareRootTest, SquareRootTest2)
 {
-    // Test the squareRoot function with the value 63924.12356
     double result = squareRoot(63924.12356);
     EXPECT_NEAR(result, 252.832204, 1e-6);
 }
 
-// Test squareRoot of 72346.18452 with 1e-6 tolerance
+// Test the squareRoot function with the value 72346.18452 with 1e-6 tolerance
 TEST(SquareRootTest, SquareRootTest3)
 {
-    // Test the squareRoot function with the value 72346.18452
     double result = squareRoot(72346.18452);
     EXPECT_NEAR(result, 268.972461, 1e-6);
 }
