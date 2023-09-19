dce3c7525c15e622f49bc3f06aef93e87819585a
pimpaoz15 <120665865+pimpaoz15@users.noreply.github.com>
2023-09-19 13:00:03 +0200

    Main.c file is modified

--- L1/main.c
+++ L1/main.c
@@ -8,7 +8,12 @@ int main()
   int res2 = IsPrime(4);
   double res3 = squareRoot(36.0);
 
+  double sqr1 = squareRoot(123.145);
+  double sqr2 = squareRoot(63924.12356);
+  double sqr3 = squareRoot(72346.18452);
+
   printf("Factorial(5)=%d, IsPrime(4)=%d, squareRoot(36.0)=%f\n", res1, res2, res3);
+  printf("squareRoot(123.145)=%f, squareRoot(63924.12356)=%f, squareRoot(72346.18452)=%f\n", sqr1, sqr2, sqr3);
 
   return 0;
 }
